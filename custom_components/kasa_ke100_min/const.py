9a2119d6a18bd5f731ee92be828e601cfd401bc9
MTTPoll <shopsiamware@gmail.com>
2025-10-03 17:23:02 +0200

    Fix UI

--- custom_components/kasa_ke100_min/const.py
+++ custom_components/kasa_ke100_min/const.py
@@ -1,12 +1,3 @@
-from datetime import timedelta
-
-DOMAIN = "kasa_ke100_min"
-MANUFACTURER = "TP-Link Kasa"
-MODEL_KE100 = "KE100 TRV"
-
-CONF_HOST = "host"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_SCAN_INTERVAL = "scan_interval"
-
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=10)
+# const.py
+DOMAIN = 'kasa_ke100_min'
+VERSION = '0.3.0'
